9ad92b0c15479048ad2ed7f6eaa0546ca0e274a8
Eric Newton <ericnewton2251@gmail.com>
2023-04-12 21:56:31 -0700

    Added old files to telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,5 +1,6 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>fa', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fo', builtin.oldfiles, {})
 vim.keymap.set('n', '<leader>p', builtin.git_files, {})
 vim.keymap.set('n', '<leader>lg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>lag', ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>", {})
