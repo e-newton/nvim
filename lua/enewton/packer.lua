d07d594626c98803f4a0721ebf3841b01a9d1eeb
Eric Newton <ericnewton2251@gmail.com>
2023-07-02 21:21:04 -0700

    Added transpaency to nvim

--- lua/enewton/packer.lua
+++ lua/enewton/packer.lua
@@ -83,5 +83,9 @@ return require('packer').startup(function(use)
   'nvim-lualine/lualine.nvim',
   requires = { 'nvim-tree/nvim-web-devicons', opt = true }
   }
+  use("xiyaowong/transparent.nvim");
+
+
+
 end)
 
